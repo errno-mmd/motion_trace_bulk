f3cce931bd3001357a25b41d744b0af04534e3a2
errno-mmd <errno-mmd@users.noreply.github.com>
2019-08-24 16:44:13 +0900

    correct OUTPUT_VIDEO_PATH

--- BulkTfposeSilent.bat
+++ BulkTfposeSilent.bat
@@ -31,7 +31,7 @@ echo 解析結果JSONディレクトリ：%OUTPUT_JSON_DIR%
 
 rem ------------------------------------------------
 rem -- 映像出力ディレクトリ
-set OUTPUT_VIDEO_PATH=%INPUT_VIDEO_DIR%%INPUT_VIDEO_FILENAME%_%DTTM%\%INPUT_VIDEO_FILENAME%_tf-pose-estimation.avi
+set OUTPUT_VIDEO_PATH=%INPUT_VIDEO_DIR%%INPUT_VIDEO_FILENAME%_%DTTM%\%INPUT_VIDEO_FILENAME%_openpose.avi
 echo 解析結果aviファイル：%OUTPUT_VIDEO_PATH%
 
 echo --------------
