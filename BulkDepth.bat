f47c79bbbba3b591ef726e92f8dca5c8f96c754a
errno-mmd <errno-mmd@users.noreply.github.com>
2020-02-01 21:03:24 +0900

    Update BulkDepth.bat

--- BulkDepth.bat
+++ BulkDepth.bat
@@ -11,6 +11,10 @@ rem -- mannequinchallenge-vmd ディレクトリに移動
 cd /d %~dp0
 cd /d %DEPTH_DIR%
 
+if not defined ORDER_START_FRAME (
+    set ORDER_START_FRAME=0
+)
+
 rem ---  python 実行
 python predict_video.py --video_path %INPUT_VIDEO% --json_path %OUTPUT_JSON_DIR% --past_depth_path "%PAST_DEPTH_PATH%" --interval 20 --reverse_specific "%REVERSE_SPECIFIC_LIST%" --order_specific "%ORDER_SPECIFIC_LIST%" --avi_output yes --verbose %VERBOSE% --number_people_max %NUMBER_PEOPLE_MAX% --end_frame_no %FRAME_END% --now %DTTM% --input single_view --batchSize 1 --order_start_frame %ORDER_START_FRAME%
 
